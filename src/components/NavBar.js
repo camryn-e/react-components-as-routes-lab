7962a29af614706e0ff46532fd947c0ff5885f68
camryn williams <williams.camryn.e@gmail.com>
2021-05-02 20:12:02 -0400

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,29 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+    <li>
+    <NavLink
+      to="/"
+    >Home</NavLink>
+    </li>
+    <li>
+    <NavLink
+      to="/movies"
+      exact
+    >Movies</NavLink>
+    </li>
+    <li>
+    <NavLink
+      to="/directors"
+    >Directors</NavLink>
+    </li>
+    <li>
+    <NavLink
+        to="/actors"
+        exact
+      >Actors</NavLink>
+    </li>
     </div>
   );
 };
